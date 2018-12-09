1cf666cd005b571d1c2d0d0bc83a9f35b4b21515
Victor Flores <suryavf@gmail.com>
2018-12-09 17:44:05 -0500

    Update pro

--- PhotoEditor.pro
+++ PhotoEditor.pro
@@ -21,20 +21,23 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # In order to do so, uncomment the following line.
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+INCLUDEPATH += /usr/local/include/opencv2
+LIBS += -L/usr/local/lib -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_videoio
+LIBS += -L$$CUDA_DIR/lib64 -lcuda -lcudart
+CONFIG += link_pkgconfig
+PKGCONFIG += opencv
 
 CONFIG += c++11
 
 SOURCES += \
         main.cpp \
         photoeditor.cpp \
-    library/bmp.cpp
-        #main.cpp \
-        #photoeditor.cpp \
+        library/bmp.cpp
 
 HEADERS += \
         photoeditor.h \
-    photoeditor.h \
-    library/bmp.h
+        photoeditor.h \
+        library/bmp.h
 
 FORMS += \
         photoeditor.ui
--- photoeditor.cpp
+++ photoeditor.cpp
@@ -1,11 +1,37 @@
 #include "photoeditor.h"
 #include "ui_photoeditor.h"
+#include <QtCore>
+#include <QtGui>
+#include <QFileDialog>
+#include <library/bmp.h>
 
 PhotoEditor::PhotoEditor(QWidget *parent) : QMainWindow(parent),
                                             ui(new Ui::PhotoEditor){
     ui->setupUi(this);
+    //scene        = new QGraphicsScene(this);
+    //graphicsView = new QGraphicsView (this);
+
+    //scene->setSceneRect( QRectF(2,2,800,800) );
 }
 
 PhotoEditor::~PhotoEditor(){
     delete ui;
+}
+
+void PhotoEditor::on_actionAbrir_triggered(){
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                 tr("Abrir Imagen BMP"), "",
+                                 tr("Imagen BMP (*.bmp);;All Files (*)"));
+    QByteArray ba = fileName.toLocal8Bit();
+    abrir_imagen(&img,ba.data());
+
+    QImage image(img.pixel, img.ancho, img.alto, QImage::Format_RGB888);
+}
+
+void PhotoEditor::on_actionGuardar_triggered(){
+    QString fileName = QFileDialog::getSaveFileName(this,
+                                 tr("Guardar Imagen BMP"), "",
+                                 tr("Imagen BMP (*.bmp);;All Files (*)"));
+    QByteArray ba = fileName.toLocal8Bit();
+    crear_imagen(&img,ba.data(),1);
 }
